759e9941dc1def21f2b3d679dda662f0d268bdb0
Tamara A. Maked <tamara.a.maked@MacBookPro.fritz.box>
2025-05-31 13:12:58 +0200

    Enable CORS for frontend requests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // backend/src/index.js
 require('dotenv').config();     // ganz oben
 const express = require('express');
+const cors = require('cors'); 
+
 const connectDB = require('./db');
 const adRoutes = require('./routes/adRoutes');
 
@@ -12,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use(express.json());
+app.use(cors());  
 app.use('/api', adRoutes);
 
 app.listen(PORT, () => {
